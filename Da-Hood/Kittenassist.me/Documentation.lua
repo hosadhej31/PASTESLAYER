66bb8c8785bdf064737e776800e91b6c06589f27
xaxa <80309202+xaxaxaxaxaxaxaxaxa@users.noreply.github.com>
2021-11-15 23:28:35 -0600

    Create Documentation.lua

---
+++ Da-Hood/Kittenassist.me/Documentation.lua
@@ -0,0 +1,7 @@
+--[[ 
+PASTESLAYER
+    $$$
+    
+-- Expose thread: https://v3rmillion.net/showthread.php?tid=1144517
+They blatantly skidded off my old Universal Aimbot called ciazware, look at that v3rmillion thread for more information
+]]--
